0dc39e7c2c8cc469a2ba6ec979e8f6ca0050daad
Liv4Music <oherbert@umich.edu>
2025-06-30 12:58:41 -0400

    missing files

---
+++ test_img.py
@@ -0,0 +1,27 @@
+import torch
+import cv2
+import torch
+import matplotlib.pyplot as plt
+
+def test_img():
+
+    # Load the model
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp/weights/last.pt', force_reload=True)
+
+    # Load and predict on image
+    img_path = '/Users/liv/Documents/Personal/RoadSign/roadsign-recognition/data/test/test1.jpeg'
+    results = model(img_path)
+
+    # Render image with bounding boxes (as list of np arrays in BGR)
+    rendered_img_bgr = results.render()[0]
+
+    # Convert BGR to RGB for matplotlib
+    rendered_img_rgb = cv2.cvtColor(rendered_img_bgr, cv2.COLOR_BGR2RGB)
+
+    # Show using matplotlib
+    plt.imshow(rendered_img_rgb)
+    plt.pause(0.001)
+    plt.axis('off')
+    plt.show()
+
+    cv2.imwrite("output_with_boxes.jpg", rendered_img_bgr)
